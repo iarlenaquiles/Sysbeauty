9c097af3f01a693d8474684f087202bb30142107
jonatas23 <jonataseduard23@gmail.com>
2018-01-09 11:28:52 -0200

    Agendamento Profissional

--- src/main/java/br/com/beleza/Repository/AgendamentoRepository.java
+++ src/main/java/br/com/beleza/Repository/AgendamentoRepository.java
@@ -10,8 +10,7 @@ import br.com.beleza.model.Agendamento;
 
 public interface AgendamentoRepository extends CrudRepository<Agendamento, Integer> {
 	
-	@Query(value = "SELECT * FROM beleza.agendamento inner join profissional_servico on agendamento.servico_servico_id = profissional_servico.servico_servico_id\r\n" + 
-			"where agendamento.usuario_usuario_id = :id ", nativeQuery = true)
+	@Query(value = "SELECT * FROM beleza.agendamento where agendamento.usuario_usuario_id = :id ", nativeQuery = true)
 	public List<Agendamento> agendamento(@Param("id") Integer id);
 	
 	@Query(value = "SELECT * FROM beleza.agendamento where ag_status != 3 and profissional_pro_id = :id", nativeQuery = true)
