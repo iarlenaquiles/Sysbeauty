e8195a1a1db52a30da26e72fb08a7c8a141f4d06
Iarlen <iarlensilva775@gmail.com>
2018-02-15 19:06:10 -0300

    Criando o model do FaleConosco

--- src/main/java/br/com/beleza/model/FaleConosco.java
+++ src/main/java/br/com/beleza/model/FaleConosco.java
@@ -1,5 +1,63 @@
 package br.com.beleza.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public class FaleConosco {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String nome;
+
+	private String email;
+
+	private String mensagem;
+
+	public FaleConosco(Integer id, String nome, String email, String mensagem) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.mensagem = mensagem;
+	}
+
+	public FaleConosco() {
+
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMensagem() {
+		return mensagem;
+	}
+
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
 
 }
