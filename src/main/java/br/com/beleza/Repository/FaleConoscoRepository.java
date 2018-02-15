f5c0f2cef8470623212c222abd7d0d23c5f65ed6
Iarlen <iarlensilva775@gmail.com>
2018-02-15 19:11:37 -0300

    Criando o repository do faleconosco

---
+++ src/main/java/br/com/beleza/Repository/FaleConoscoRepository.java
@@ -0,0 +1,9 @@
+package br.com.beleza.Repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.beleza.model.FaleConosco;
+
+public interface FaleConoscoRepository extends CrudRepository<FaleConosco, Integer>{
+
+}
