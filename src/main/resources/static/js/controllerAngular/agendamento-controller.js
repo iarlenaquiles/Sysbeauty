7a132d447c7caee63330a66614a22108f1657e20
Iarlen <iarlensilva775@gmail.com>
2018-02-16 17:04:12 -0300

    Criando o btn para mostrar a data que será escolhida

--- src/main/resources/static/js/controllerAngular/agendamento-controller.js
+++ src/main/resources/static/js/controllerAngular/agendamento-controller.js
@@ -1,9 +1,7 @@
 service.controller('agendamentoProfissional', function agendamentoProfissional($scope, $http) {
 	
-	
-	$scope.Markers = [];
-	
-	$scope.profissionalSelecionado;
+	$scope.dataAgendamento;
+	$scope.horaAgendamento;
 	$scope.servicos = [];
 	$scope.servicosSelecionados = [
 		{
@@ -13,162 +11,8 @@ service.controller('agendamentoProfissional', function agendamentoProfissional($
 			promocao: {}
 		}
 	];
-	
-	$scope.longitude;
-	$scope.latitude;
 
 	$scope.constructor = function() {
-		
-	}
-	
-	//Setting the Map options.
-    $scope.MapOptions = {
-        center: new google.maps.LatLng('-16.616418', '-49.226201'),
-        zoom: 0,
-        mapTypeId: google.maps.MapTypeId.ROADMAP
-    };
-	
-	//Initializing the InfoWindow, Map and LatLngBounds objects.
-    $scope.InfoWindow = new google.maps.InfoWindow();
-    $scope.Latlngbounds = new google.maps.LatLngBounds();
-    $scope.Map = new google.maps.Map(document.getElementById("dvMap"), $scope.MapOptions);
-	
-	/*$scope.gerarLocalizacao = function(profissionais) {
-		for(let p of profissionais) {
-			var geocoder;
-			geocoder = new google.maps.Geocoder();
-			var lat = '';
-			var lng = '';
-			var address = p.cep;
-			
-			geocoder.geocode({ address }, function(results, status) {
-				if (status == google.maps.GeocoderStatus.OK) {
-					lat = results[0].geometry.location.lat();
-					lng = results[0].geometry.location.lng();
-					
-					var latlng = new google.maps.LatLng(lat, lng);
-					
-					//Initializing the Marker object.
-					var marker = new google.maps.Marker({
-						position: latlng,
-						map: $scope.Map,
-						title: p.nome
-					});
-			        
-			        marker.addListener('click', function() {
-      		          $scope.Map.setZoom(17);
-      		          $scope.Map.setCenter(marker.getPosition());
-      		          // / retorna o perfil do usuario clicado
-      		          perfilPro(j);
-      		          
-      		        });
-			        
-			        //Plotting the Marker on the Map.
-			        $scope.Latlngbounds.extend(marker.position);
-				} else {
-					alert("Não foi possível obter localização: " + status);
-				}
-			})
-		}
-	}*/
-    
-    $scope.gerarLocalizacao = function(profissionais) {
-    	for(let p of profissionais) {
-    	   var geocoder;
-    	   geocoder = new google.maps.Geocoder();
-    	   var lat = '';
-    	   var lng = '';
-    	   var address = p.cep;
-    	   
-    	   geocoder.geocode({ address }, function(results, status) {
-    		   if (status == google.maps.GeocoderStatus.OK) {
-    			   lat = results[0].geometry.location.lat();
-    			   lng = results[0].geometry.location.lng();
-    	          
-    			   var myLatlng = new google.maps.LatLng(lat, lng);
-    	        
-    			   //Initializing the Marker object.
-    			   var marker = new google.maps.Marker({
-    				   position: myLatlng,
-    				   map: $scope.Map,
-    				   title: p.nome
-    			   });
-
-    			   //Adding InfoWindow to the Marker.
-    			   (function (marker, p) {
-    				   google.maps.event.addListener(marker, "click", function (e) {
-	    	               $scope.InfoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + p.nome + "</div>");
-	    	               $scope.InfoWindow.open($scope.Map, marker);
-    				   });
-    			   })(marker, p);
-    	           
-    	           marker.addListener('click', function() {
-    	        	   $scope.Map.setZoom(17);
-    	        	   $scope.Map.setCenter(marker.getPosition());
-    	                  // / retorna o perfil do usuario clicado
-    	                  perfilPro(p);
-    	                  
-    	                });
-    	           
-    	           //Plotting the Marker on the Map.
-    	           $scope.Latlngbounds.extend(marker.position);
-    	           //console.log(marker.position);
-    	    } else {
-    	     alert("Não foi possível obter localização: " + status);
-    	    }
-    	   });
-    	}
-    	
-		$scope.Map.setCenter($scope.Latlngbounds.getCenter());
-		$scope.Map.fitBounds($scope.Latlngbounds);
-    }
-	
-	$scope.listarProfissionais = function() {
-    	$http({
-			method: 'GET',
-			url: '/listPro'
-		}).then(function successCallback(response) {
-			console.log("Success");
-			$scope.gerarLocalizacao(response.data);
-		}, function errorCallback(response) {
-			console.log("ERROR" + response.data);
-		});
-	}
-	
-	function perfilPro(pro){
-	    $http({
-	    	method: 'GET',
-	    	url: '/perfilPro/'+pro.id
-	    }).then(function successCallback(response) {
-	    	console.log("Success");
-	    	
-	    	var labe1= document.getElementById('labe1');
-	        labe1.innerHTML  = response.data.nome;
-	        
-	        $scope.profissionalSelecionado = pro;
-	        i = $scope.profissionalSelecionado.servico;
-	        
-	        for (cont = 0; cont < i.length; cont++) {
-	        	console.log('Teste ' + cont);
-	        	console.log(i[cont].id);
-	        	$scope.servicos.push({
-	        		id: i[cont].id,
-	    			servico: i[cont].servico,
-	    			valor: i[cont].valor,
-	    			checked: '',
-	    			promocao: i[cont].promocao
-	        	});
-	        }
-	        
-	        console.log($scope.servicos);
-	        
-	        
-	        console.log($scope.profissionalSelecionado);
-	    	
-	    }, function errorLog(response) {
-	    	console.log("ERROR " + response.data);
-	    });
-		
 		
 	}
 	
@@ -196,40 +40,11 @@ service.controller('agendamentoProfissional', function agendamentoProfissional($
 	$scope.constructor();
 	$scope.listarProfissionais();
 	
-
-    
-
-    //Looping through the Array and adding Markers.
-    /*for (var i = 0; i < $scope.Markers.length; i++) {
-        var data = $scope.Markers[i];
-        var myLatlng = new google.maps.LatLng(data.lat, data.lng);
-        
-
-        //Initializing the Marker object.
-        var marker = new google.maps.Marker({
-            position: myLatlng,
-            map: $scope.Map,
-            title: data.title
-        });
-
-        //Adding InfoWindow to the Marker.
-        (function (marker, data) {
-            google.maps.event.addListener(marker, "click", function (e) {
-                $scope.InfoWindow.setContent("<div style = 'width:200px;min-height:40px'>" + data.description + "</div>");
-                $scope.InfoWindow.open($scope.Map, marker);
-            });
-        })(marker, data);
-
-        //Plotting the Marker on the Map.
-        $scope.Latlngbounds.extend(marker.position);
-    }*/
-
-    //Adjusting the Map for best display.
-    
-    /* =============================================== Agendamento =============================================== */
-	
-	
 	$scope.servicosSelecionados = [];
+	
+	$scope.btnProximo = function() {
+		console.log($scope.dataAgendamento);
+	}
 	
 	$scope.btnAgendar = function() {
 		console.log($scope.servicos);
