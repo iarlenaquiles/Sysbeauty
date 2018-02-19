10873307f8832223583abaf6b05aec703b6bc382
Iarlen <iarlensilva775@gmail.com>
2018-02-19 13:24:54 -0300

    Removendo importação desnecessária

--- src/main/java/br/com/beleza/model/Usuario.java
+++ src/main/java/br/com/beleza/model/Usuario.java
@@ -2,16 +2,12 @@ package br.com.beleza.model;
 
 import java.io.Serializable;
 import java.util.Date;
-import java.util.Set;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.NamedQuery;
 
