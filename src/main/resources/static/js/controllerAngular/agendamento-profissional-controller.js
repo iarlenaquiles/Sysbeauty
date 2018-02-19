f60892968be7888e1f38fb77ce8cc6e02ac036c1
Iarlen <iarlensilva775@gmail.com>
2018-02-19 15:17:24 -0300

    Criando a lista de atendimentos

--- src/main/resources/static/js/controllerAngular/agendamento-profissional-controller.js
+++ src/main/resources/static/js/controllerAngular/agendamento-profissional-controller.js
@@ -1,8 +1,10 @@
 service.controller('agendamentoProfissional', function agendamentoProfissional($scope, $http) {
 	
-		
-		console.log("controller do profissional agendamento");
-		
-	
+
+    	$scope.atendimentos = [
+        	{cliente: 'Marilene', dia: '19/02/2018', local: 'Rua teste'},
+        	{cliente: 'Ivone', dia: '20/02/2018', local: 'Rua Teste'}
+        ];
+
 	
 });
