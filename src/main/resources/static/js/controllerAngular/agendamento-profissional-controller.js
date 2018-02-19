7190a11b341ece1cf4daaf66f5d34b249da4b94f
Iarlen <iarlensilva775@gmail.com>
2018-02-19 18:59:56 -0300

    Criando um novo arquivo para guardar o modal

--- src/main/resources/static/js/controllerAngular/agendamento-profissional-controller.js
+++ src/main/resources/static/js/controllerAngular/agendamento-profissional-controller.js
@@ -1,31 +1,47 @@
-service.controller('agendamentoProfissional', function agendamentoProfissional($scope, $http) {
-	
-		$scope.cancelar = false;
+service.controller('agendamentoProfissional', function agendamentoProfissional(
+		$scope, $http) {
 
-    	$scope.agendamentos = [
-        	{cliente: 'Marilene', dia: '19/02/2018', local: 'Rua teste'},
-        	{cliente: 'Ivone', dia: '20/02/2018', local: 'Rua Teste'}
-        ];
+	$scope.cancelar = false;
 
-    	
-    	function listaAgendamentos($scope) {
-    		$scope.fetchProductsList = function() {
-        		$http.get('url').success(function(agendamentos){
-            		$scope.agendamentos = agendamentos;
-        		});
-    		}
-    	}
-	
-    	$scope.cancelar = function(index) {
-    		$scope.agendamento = $scope.agendamentos[index];
-    		$scope.cancelar = true;
-    	}
-    	
-    	$scope.chat = function() {
-    		console.log("chat");
-    	}
-    	
-    	$scope.agendamentosDia = function(data) {
-    		console.log(data);
-    	}
+	$scope.agendamentos = [ {
+		cliente : 'Marilene',
+		dia : '19/02/2018',
+		local : 'Rua teste'
+	}, {
+		cliente : 'Ivone',
+		dia : '20/02/2018',
+		local : 'Rua Teste'
+	} ];
+
+	var modal = {
+		controller : 'CancelarController',
+		templateUrl : '../../templates/prof-pages/cancelamentoProfissional.html'
+	};
+
+	function listaAgendamentos($scope) {
+		$scope.fetchProductsList = function() {
+			$http.get('url').success(function(agendamentos) {
+				$scope.agendamentos = agendamentos;
+			});
+		}
+	}
+
+	$scope.cancelar = function(agendamento) {
+		
+	}
+
+	$scope.chat = function() {
+		console.log("chat");
+	}
+
+	$scope.agendamentosDia = function(data) {
+		console.log(data);
+	}
 });
+
+function CancelarController($scope){
+	  
+	  console.log("entrou controller cancelamento");
+}
+
+
