433f4e62e746c3e6579c6e0d5eb27e99a94ca5a9
Iarlen <iarlensilva775@gmail.com>
2018-02-21 18:57:25 -0300

    Criando o controller da promoção

--- src/main/java/com/beleza/controller/PromocaoController.java
+++ src/main/java/com/beleza/controller/PromocaoController.java
@@ -1,8 +1,47 @@
 package com.beleza.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.beleza.model.Promocao;
+import com.beleza.service.PromocaoService;
 
 @RestController
 public class PromocaoController {
 
+	@Autowired
+	PromocaoService promocaoService;
+
+	@GetMapping("/promocoes")
+	public List<Promocao> listar() {
+		return this.promocaoService.listaPromocao();
+	}
+
+	@GetMapping("/promocoes/{id}")
+	public Promocao getById(@PathVariable Integer id) {
+		return this.promocaoService.getById(id);
+	}
+
+	@PostMapping("/clientes")
+	public Promocao salvar(@RequestBody Promocao promocao) {
+		return this.promocaoService.salvarPromocao(promocao);
+	}
+
+	@PutMapping("/promocoes")
+	public Promocao editar(@RequestBody Promocao promocao) {
+		return this.promocaoService.salvarPromocao(promocao);
+	}
+
+	@DeleteMapping("/promocoes/{id}")
+	public void deletar(@PathVariable Integer id) {
+		this.promocaoService.deletePromocao(id);
+	}
 }
