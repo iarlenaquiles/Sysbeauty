779663f63d248ff4aa5999241133e8efbd5415a7
Iarlen <iarlensilva775@gmail.com>
2018-02-21 18:48:50 -0300

    Criando o controller do cancelamento

--- src/main/java/com/beleza/controller/CancelamentoController.java
+++ src/main/java/com/beleza/controller/CancelamentoController.java
@@ -1,8 +1,47 @@
 package com.beleza.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.beleza.model.Cancelamento;
+import com.beleza.service.CancelamentoService;
 
 @RestController
 public class CancelamentoController {
 
+	@Autowired
+	CancelamentoService cancelamentoService;
+
+	@GetMapping("/cancelamentos")
+	public List<Cancelamento> listar() {
+		return this.cancelamentoService.listaCancelamentos();
+	}
+
+	@GetMapping("/cancelamentos/{id}")
+	public Cancelamento getById(@PathVariable Integer id) {
+		return this.cancelamentoService.getById(id);
+	}
+
+	@PostMapping("/cancelamentos")
+	public Cancelamento salvar(@RequestBody Cancelamento cancelamento) {
+		return this.cancelamentoService.salvarCancelamento(cancelamento);
+	}
+
+	@PutMapping("/cancelamentos")
+	public Cancelamento editar(@RequestBody Cancelamento cancelamento) {
+		return this.cancelamentoService.salvarCancelamento(cancelamento);
+	}
+
+	@DeleteMapping("/cancelamentos/{id}")
+	public void deletar(@PathVariable Integer id) {
+		this.cancelamentoService.deleteCancelamento(id);
+	}
 }
