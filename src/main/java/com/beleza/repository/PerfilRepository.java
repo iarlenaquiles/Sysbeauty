2b0e74b52010c158d5f70cbdb319d069a6c14edd
Iarlen <iarlensilva775@gmail.com>
2018-02-22 15:16:24 -0300

    Criando os repositórios do usuário e do perfil

---
+++ src/main/java/com/beleza/repository/PerfilRepository.java
@@ -0,0 +1,9 @@
+package com.beleza.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beleza.model.Perfil;
+
+public interface PerfilRepository extends JpaRepository<Perfil, Integer>{
+
+}
