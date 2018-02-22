5815f8619ec2c25bdc2cac64d788b0bc85416a11
Iarlen <iarlensilva775@gmail.com>
2018-02-22 17:14:14 -0300

    Criando service do perfil

--- src/main/java/com/beleza/service/PerfilService.java
+++ src/main/java/com/beleza/service/PerfilService.java
@@ -1,5 +1,33 @@
 package com.beleza.service;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.beleza.model.Perfil;
+import com.beleza.repository.PerfilRepository;
+
+@Service
 public class PerfilService {
+	
+	@Autowired
+	private PerfilRepository perfilRepository;
+	
+	public List<Perfil> listaPerfil() {
+		return perfilRepository.findAll();
+	}
+	
+	public Perfil salvarPerfil(Perfil perfil) {
+		return perfilRepository.save(perfil);
+	}
+	
+	public void deletePerfil(Integer id) {
+		perfilRepository.delete(id);
+	}
+	
+	public Perfil getById(Integer id) {
+		return perfilRepository.findOne(id);
+	}
 
 }
