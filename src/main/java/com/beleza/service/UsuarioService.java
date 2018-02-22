8fcd7e2f3a6800007b5c28a8267664e5b5081dee
Iarlen <iarlensilva775@gmail.com>
2018-02-22 17:10:23 -0300

    Criando service do usuario

--- src/main/java/com/beleza/service/UsuarioService.java
+++ src/main/java/com/beleza/service/UsuarioService.java
@@ -1,5 +1,32 @@
 package com.beleza.service;
 
-public class UsuarioService {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.beleza.model.Usuario;
+import com.beleza.repository.UsuarioRepository;
+
+@Service
+public class UsuarioService {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepository;
+
+	public List<Usuario> listaUsuario(){
+		return usuarioRepository.findAll();
+	}
+	
+	public Usuario salvarUsuario(Usuario usuario) {
+		return usuarioRepository.save(usuario);
+	}
+	
+	public void deletarUsuario(Integer id) {
+		usuarioRepository.delete(id);
+	}
+	
+	public Usuario getById(Integer id) {
+		return usuarioRepository.findOne(id);
+	}
 }
