5cd51959ceda4648a9442dc9ac820168f7483304
Iarlen <iarlensilva775@gmail.com>
2018-02-22 17:21:52 -0300

    Criando o controller do usuário

--- src/main/java/com/beleza/controller/UsuarioController.java
+++ src/main/java/com/beleza/controller/UsuarioController.java
@@ -1,5 +1,48 @@
 package com.beleza.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.beleza.model.Usuario;
+import com.beleza.service.UsuarioService;
+
+@RestController
 public class UsuarioController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@GetMapping("/usuarios")
+	public List<Usuario> listar() {
+		return this.usuarioService.listaUsuario();
+	}
+	
+	@GetMapping("/usuarios/{id}")
+	public Usuario getById(@PathVariable Integer id) {
+		return this.usuarioService.getById(id);
+	}
+	
+	@PostMapping("/usuarios")
+	public Usuario salvar(@RequestBody Usuario usuario) {
+		return this.usuarioService.salvarUsuario(usuario);
+	}
+	
+	@PutMapping("/usuarios")
+	public Usuario editar(@RequestBody Usuario usuario) {
+		return this.usuarioService.salvarUsuario(usuario);
+	}
+	
+	@DeleteMapping("/usuarios/{id}")
+	public void deletar(@PathVariable Integer id) {
+		this.usuarioService.deletarUsuario(id);
+	}
 
 }
