96e7888920835819d504b2e638e8ae39d1c2fe3b
Iarlen <iarlensilva775@gmail.com>
2018-02-22 15:08:23 -0300

    Criando a model de usuário

---
+++ src/main/java/com/beleza/model/Usuario.java
@@ -0,0 +1,73 @@
+package com.beleza.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Usuario implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String email;
+
+	private String senha;
+
+	@OneToOne
+	private Perfil perfil;
+
+	public Usuario(Integer id, String email, String senha, Perfil perfil) {
+		this.id = id;
+		this.email = email;
+		this.senha = senha;
+		this.perfil = perfil;
+	}
+
+	public Usuario() {
+
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Perfil getPerfil() {
+		return perfil;
+	}
+
+	public void setPerfil(Perfil perfil) {
+		this.perfil = perfil;
+	}
+
+}
