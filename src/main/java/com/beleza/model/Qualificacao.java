60f75d792078b071ea6e1cee9c870e53f4cddc27
Iarlen <iarlensilva775@gmail.com>
2018-02-23 15:25:53 -0300

    Criando o model da qualificação

---
+++ src/main/java/com/beleza/model/Qualificacao.java
@@ -0,0 +1,15 @@
+package com.beleza.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Qualificacao implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
