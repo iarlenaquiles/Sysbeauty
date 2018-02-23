3f05412f083b9a7388c67958fbaab9ff02db711d
Iarlen <iarlensilva775@gmail.com>
2018-02-23 18:09:41 -0300

    Criando o model da compra do pacote

---
+++ src/main/java/com/beleza/model/ComprarPacote.java
@@ -0,0 +1,59 @@
+package com.beleza.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class ComprarPacote implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@ManyToOne
+	private Pacote pacote;
+
+	@ManyToOne
+	private Cliente cliente;
+
+	public ComprarPacote(Integer id, Pacote pacote, Cliente cliente) {
+		this.id = id;
+		this.pacote = pacote;
+		this.cliente = cliente;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Pacote getPacote() {
+		return pacote;
+	}
+
+	public void setPacote(Pacote pacote) {
+		this.pacote = pacote;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+}
