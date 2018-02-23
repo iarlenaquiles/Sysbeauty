feb07d3d44d720111442e01ae60f8b62ba53dee0
Iarlen <iarlensilva775@gmail.com>
2018-02-23 15:31:40 -0300

    Criando o model da qualificação

--- src/main/java/com/beleza/model/Qualificacao.java
+++ src/main/java/com/beleza/model/Qualificacao.java
@@ -3,6 +3,10 @@ package com.beleza.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 
 @Entity
 public class Qualificacao implements Serializable {
@@ -11,5 +15,44 @@ public class Qualificacao implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@OneToMany
+	private Profissional profissional;
+
+	private String comentario;
+
+	public Qualificacao(Integer id, Profissional profissional, String comentario) {
+		this.id = id;
+		this.profissional = profissional;
+		this.comentario = comentario;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Profissional getProfissional() {
+		return profissional;
+	}
+
+	public void setProfissional(Profissional profissional) {
+		this.profissional = profissional;
+	}
+
+	public String getComentario() {
+		return comentario;
+	}
+
+	public void setComentario(String comentario) {
+		this.comentario = comentario;
+	}
 
 }
