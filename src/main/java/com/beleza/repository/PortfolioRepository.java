56e422ba1e845ac03120188f04ffbb56a829eed9
Iarlen <iarlensilva775@gmail.com>
2018-02-23 18:44:48 -0300

    Criando os repositorios do portfolio e do selo

---
+++ src/main/java/com/beleza/repository/PortfolioRepository.java
@@ -0,0 +1,9 @@
+package com.beleza.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beleza.model.Portfolio;
+
+public interface PortfolioRepository extends JpaRepository<Portfolio, Integer>{
+
+}
---
+++ src/main/java/com/beleza/repository/SeloRepository.java
@@ -0,0 +1,9 @@
+package com.beleza.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beleza.model.Selo;
+
+public interface SeloRepository extends JpaRepository<Selo, Integer>{
+
+}
