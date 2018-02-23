2390f8287069c08bb969a0d6f354964c2936b88f
Iarlen <iarlensilva775@gmail.com>
2018-02-23 18:10:48 -0300

    Criando o repositorio da compra do pacote

---
+++ src/main/java/com/beleza/repository/CompraPacoteRepository.java
@@ -0,0 +1,9 @@
+package com.beleza.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beleza.model.ComprarPacote;
+
+public interface CompraPacoteRepository extends JpaRepository<ComprarPacote, Integer>{
+
+}
