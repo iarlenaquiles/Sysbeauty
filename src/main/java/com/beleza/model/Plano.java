9dca736ddcdf61179a4e33e6d8171726f9eee5b7
Iarlen <iarlensilva775@gmail.com>
2018-02-23 15:57:22 -0300

    Criando o model do plano

---
+++ src/main/java/com/beleza/model/Plano.java
@@ -0,0 +1,56 @@
+package com.beleza.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Plano implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	public Integer id;
+
+	private String nome;
+
+	private String Descricao;
+
+	public Plano(Integer id, String nome, String descricao) {
+		this.id = id;
+		this.nome = nome;
+		Descricao = descricao;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return Descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		Descricao = descricao;
+	}
+
+}
