7e48c7950e4c2e8da09a3ac6c1f1b8b88bc9d273
Iarlen <iarlensilva775@gmail.com>
2018-02-23 15:58:25 -0300

    Criando o repository do plano

---
+++ src/main/java/com/beleza/repository/PlanoRepository.java
@@ -0,0 +1,9 @@
+package com.beleza.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beleza.model.Plano;
+
+public interface PlanoRepository extends JpaRepository<Plano, Integer>{
+
+}
