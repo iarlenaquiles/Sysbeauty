0810a2c715bc6b1209b253c82695c8f5dc594af8
Iarlen <iarlensilva775@gmail.com>
2018-02-23 18:37:55 -0300

    Criando o model do selo

--- src/main/java/com/beleza/model/Selo.java
+++ src/main/java/com/beleza/model/Selo.java
@@ -3,6 +3,9 @@ package com.beleza.model;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Selo implements Serializable {
@@ -11,9 +14,54 @@ public class Selo implements Serializable {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	
-	
-	
-	
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String nome;
+
+	private String descricao;
+
+	private String foto;
+
+	public Selo(Integer id, String nome, String descricao, String foto) {
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.foto = foto;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getFoto() {
+		return foto;
+	}
+
+	public void setFoto(String foto) {
+		this.foto = foto;
+	}
 
 }
