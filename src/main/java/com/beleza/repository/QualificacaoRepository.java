2b69aa8b1de31d6b2060d26b99ac84611c81211e
Iarlen <iarlensilva775@gmail.com>
2018-02-23 15:34:32 -0300

    Criando o repositório da qualificação

---
+++ src/main/java/com/beleza/repository/QualificacaoRepository.java
@@ -0,0 +1,9 @@
+package com.beleza.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beleza.model.Qualificacao;
+
+public interface QualificacaoRepository extends JpaRepository<Qualificacao, Integer>{
+
+}
