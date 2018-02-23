a7a2ac1d28ad00d848bea613bd110938dea004c7
Iarlen <iarlensilva775@gmail.com>
2018-02-23 18:35:35 -0300

    Criando o model do selo

---
+++ src/main/java/com/beleza/model/Selo.java
@@ -0,0 +1,19 @@
+package com.beleza.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Selo implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	
+	
+
+}
