884896811381cb6031550571254070c46eaf539e
Iarlen <iarlensilva775@gmail.com>
2018-02-23 18:07:48 -0300

    Adicionando o entity ao pacote

--- src/main/java/com/beleza/model/Pacote.java
+++ src/main/java/com/beleza/model/Pacote.java
@@ -3,12 +3,14 @@ package com.beleza.model;
 import java.io.Serializable;
 import java.util.List;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 
+@Entity
 public class Pacote implements Serializable {
 
 	/**
