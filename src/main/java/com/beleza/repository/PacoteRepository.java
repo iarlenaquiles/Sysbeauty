ef2620dcd6af9810692f4774fdbba94158d83864
Iarlen <iarlensilva775@gmail.com>
2018-02-23 17:29:42 -0300

    Criando o repository do pacote

---
+++ src/main/java/com/beleza/repository/PacoteRepository.java
@@ -0,0 +1,9 @@
+package com.beleza.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beleza.model.Pacote;
+
+public interface PacoteRepository extends JpaRepository<Pacote, Integer>{
+
+}
