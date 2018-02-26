4d434d546b78062412445e25b388bf1514e9c658
Iarlen <iarlensilva775@gmail.com>
2018-02-26 15:50:15 -0300

    Criando o model do moderador

---
+++ src/main/java/com/beleza/model/Moderador.java
@@ -0,0 +1,67 @@
+package com.beleza.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Moderador implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	private String nome;
+
+	private String email;
+
+	private boolean status;
+
+	public Moderador(Integer id, String nome, String email, boolean status) {
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.status = status;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+}
