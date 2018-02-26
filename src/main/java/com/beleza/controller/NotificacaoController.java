16947d650f9400ce66a23ea53d650567308009c6
Iarlen <iarlensilva775@gmail.com>
2018-02-26 16:59:45 -0300

    Criando o controller da notificação

---
+++ src/main/java/com/beleza/controller/NotificacaoController.java
@@ -0,0 +1,48 @@
+package com.beleza.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.beleza.model.Notificacao;
+import com.beleza.service.NotificacaoService;
+
+@RestController
+public class NotificacaoController {
+
+	@Autowired
+	NotificacaoService notificacaoService;
+
+	@GetMapping("/notificacoes")
+	public List<Notificacao> listar() {
+		return this.notificacaoService.listaNotificacoes();
+	}
+
+	@GetMapping("/notificacoes/{id}")
+	public Notificacao getById(@PathVariable Integer id) {
+		return this.notificacaoService.getById(id);
+	}
+
+	@PostMapping("/notificacoes")
+	public Notificacao salvar(@RequestBody Notificacao notificacao) {
+		return this.notificacaoService.salvarNotificacao(notificacao);
+	}
+
+	@PutMapping("/notificacoes")
+	public Notificacao editar(@RequestBody Notificacao notificacao) {
+		return this.notificacaoService.salvarNotificacao(notificacao);
+	}
+
+	@DeleteMapping("/notificacoes/{id}")
+	public void deletar(@PathVariable Integer id) {
+		this.notificacaoService.deleteNotificacao(id);
+	}
+
+}
