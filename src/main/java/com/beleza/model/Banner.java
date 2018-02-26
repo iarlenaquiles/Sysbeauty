425ad4f215f6befdda64cb68becc00260bff8b3a
Iarlen <iarlensilva775@gmail.com>
2018-02-26 16:18:26 -0300

    Criando o model do banner

---
+++ src/main/java/com/beleza/model/Banner.java
@@ -0,0 +1,76 @@
+package com.beleza.model;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Banner implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Integer id;
+
+	private String nome;
+
+	private String imagem;
+
+	private Date dataInserido;
+
+	private TipoPublicacao tipo;
+
+	public Banner(Integer id, String nome, String imagem, Date dataInserido, TipoPublicacao tipo) {
+		this.id = id;
+		this.nome = nome;
+		this.imagem = imagem;
+		this.dataInserido = dataInserido;
+		this.tipo = tipo;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getImagem() {
+		return imagem;
+	}
+
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	public Date getDataInserido() {
+		return dataInserido;
+	}
+
+	public void setDataInserido(Date dataInserido) {
+		this.dataInserido = dataInserido;
+	}
+
+	public TipoPublicacao getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(TipoPublicacao tipo) {
+		this.tipo = tipo;
+	}
+
+}
