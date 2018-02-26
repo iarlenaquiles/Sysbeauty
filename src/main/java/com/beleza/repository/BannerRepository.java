ba2abd3f64758f7b8560ccde639c843929540fa4
Iarlen <iarlensilva775@gmail.com>
2018-02-26 16:18:51 -0300

    Criando o repository do banner

---
+++ src/main/java/com/beleza/repository/BannerRepository.java
@@ -0,0 +1,9 @@
+package com.beleza.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beleza.model.Banner;
+
+public interface BannerRepository extends JpaRepository<Banner, Integer>{
+
+}
