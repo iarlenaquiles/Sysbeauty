bb8ed4c04e368b9d0092eb43c0f450bffa9dc130
Iarlen <iarlensilva775@gmail.com>
2018-02-26 15:51:58 -0300

    Criando o repository do moderador

---
+++ src/main/java/com/beleza/repository/ModeradorRepository.java
@@ -0,0 +1,9 @@
+package com.beleza.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beleza.model.Moderador;
+
+public interface ModeradorRepository extends JpaRepository<Moderador, Integer> {
+
+}
