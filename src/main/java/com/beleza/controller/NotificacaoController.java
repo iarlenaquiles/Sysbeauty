7a0c7c147be8511f95d7714ec99e34ab23adc210
Iarlen <iarlensilva775@gmail.com>
2018-02-26 20:50:13 -0300

    Criando a notificação

--- src/main/java/com/beleza/controller/NotificacaoController.java
+++ src/main/java/com/beleza/controller/NotificacaoController.java
@@ -1,8 +1,15 @@
 package com.beleza.controller;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,6 +24,12 @@ import com.beleza.service.NotificacaoService;
 @RestController
 public class NotificacaoController {
 
+	private final String TOPIC_CLIENTE = "Cliente";
+	
+	private final String TOPIC_PROFISSIONAL = "Profissional";
+	
+	private final String TOPIC_AMBOS = "Ambos";
+	
 	@Autowired
 	NotificacaoService notificacaoService;
 
@@ -33,11 +46,11 @@ public class NotificacaoController {
 	@PostMapping("/notificacoes")
 	public Notificacao salvar(@RequestBody Notificacao notificacao) {
 		if (notificacao.getTipo().getDescricao() == "PROFISSIONAIS") {
-			System.out.println("Enviar push para o profissional");
+			this.send(notificacao, TOPIC_PROFISSIONAL);
 		} else if (notificacao.getTipo().getDescricao() == "CLIENTES"){
-			System.out.println("Enviar push para o cliente");
+			this.send(notificacao, TOPIC_CLIENTE);
 		} else {
-			System.out.println("Enviar push para ambos");
+			this.send(notificacao, TOPIC_AMBOS);
 		}
 		
 		return this.notificacaoService.salvarNotificacao(notificacao);
@@ -51,6 +64,36 @@ public class NotificacaoController {
 	@DeleteMapping("/notificacoes/{id}")
 	public void deletar(@PathVariable Integer id) {
 		this.notificacaoService.deleteNotificacao(id);
+	}
+	
+	public ResponseEntity<String> send(Notificacao notificacao, String topico) throws JSONException {
+
+		JSONObject body = new JSONObject();
+		body.put("to", "/topics/" + topico);
+		body.put("priority", "high");
+
+		JSONObject notification = new JSONObject();
+		notification.put("title", "Disque beleza");
+		notification.put("body", notificacao.getMensagem());
+
+		body.put("notification", notification);
+
+		HttpEntity<String> request = new HttpEntity<>(body.toString());
+
+		CompletableFuture<String> pushNotification = notificacaoService.send(request);
+		CompletableFuture.allOf(pushNotification).join();
+
+		try {
+			String firebaseResponse = pushNotification.get();
+			
+			return new ResponseEntity<>(firebaseResponse, HttpStatus.OK);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} catch (ExecutionException e) {
+			e.printStackTrace();
+		}
+
+		return new ResponseEntity<>("Push Notification ERROR!", HttpStatus.BAD_REQUEST);
 	}
 
 }
