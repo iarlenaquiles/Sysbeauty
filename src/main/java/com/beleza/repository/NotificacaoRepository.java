34c1c13b1e08a3c387ed362453cf06fb70228e73
Iarlen <iarlensilva775@gmail.com>
2018-02-26 16:52:21 -0300

    Criando o repository da notificação

---
+++ src/main/java/com/beleza/repository/NotificacaoRepository.java
@@ -0,0 +1,9 @@
+package com.beleza.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beleza.model.Notificacao;
+
+public interface NotificacaoRepository extends JpaRepository<Notificacao, Integer> {
+
+}
