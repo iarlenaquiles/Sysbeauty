a9ea7e044e24abd1e7625f17faa4f1f302f0f75b
Iarlen <iarlensilva775@gmail.com>
2018-02-27 19:48:45 -0300

    Inserindo o generatedvalue

--- src/main/java/com/beleza/model/ResetSenha.java
+++ src/main/java/com/beleza/model/ResetSenha.java
@@ -5,6 +5,8 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -20,6 +22,7 @@ public class ResetSenha implements Serializable {
 	private static final int EXPIRACAO = 60 * 24;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String token;
