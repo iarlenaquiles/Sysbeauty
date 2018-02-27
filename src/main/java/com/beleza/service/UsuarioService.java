e144aa3b35800512003980c59b20561d7746d72c
Iarlen <iarlensilva775@gmail.com>
2018-02-27 14:50:14 -0300

    Retirado a criptografia do serviço

--- src/main/java/com/beleza/service/UsuarioService.java
+++ src/main/java/com/beleza/service/UsuarioService.java
@@ -3,7 +3,6 @@ package com.beleza.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.beleza.model.Usuario;
@@ -15,17 +14,11 @@ public class UsuarioService {
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
-	@Autowired
-	private BCryptPasswordEncoder bCryptPasswordEncoder;
-
 	public List<Usuario> listaUsuario() {
 		return usuarioRepository.findAll();
 	}
 
 	public Usuario salvarUsuario(Usuario usuario) {
-		usuario.setEmail(usuario.getEmail());
-		usuario.setPerfil(usuario.getPerfil());
-		usuario.setSenha(bCryptPasswordEncoder.encode(usuario.getSenha()));
 		return usuarioRepository.save(usuario);
 	}
 
