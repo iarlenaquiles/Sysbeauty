9cac9c642aa7f667ae0345c921aa7bf40c33426b
Iarlen <iarlensilva775@gmail.com>
2018-02-27 18:19:18 -0300

    Criando o reset da senha

--- src/main/java/com/beleza/controller/UsuarioController.java
+++ src/main/java/com/beleza/controller/UsuarioController.java
@@ -1,14 +1,17 @@
 package com.beleza.controller;
 
 import java.util.List;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.beleza.model.Usuario;
@@ -44,5 +47,22 @@ public class UsuarioController {
 	public void deletar(@PathVariable Integer id) {
 		this.usuarioService.deletarUsuario(id);
 	}
+	
+	@PostMapping("/usuarios/reset")
+	public String resetSenha(@RequestParam("email") String email) {
+		Usuario usuario = this.usuarioService.getByEmail(email);
+		
+		if (usuario == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		
+		String token = UUID.randomUUID().toString();
+		this.usuarioService.createResetSenhaUsuario(usuario, token);
+		
+		//Criar mailsender
+		return "Email enviado";
+	}
+	
+	
 
 }
