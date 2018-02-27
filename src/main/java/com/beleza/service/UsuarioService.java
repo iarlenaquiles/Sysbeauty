33ea10bc16a15dcbaed1e643922a0c7fca22b0b3
Iarlen <iarlensilva775@gmail.com>
2018-02-27 14:04:41 -0300

    Criptografando a senha do usuário

--- src/main/java/com/beleza/service/UsuarioService.java
+++ src/main/java/com/beleza/service/UsuarioService.java
@@ -3,6 +3,7 @@ package com.beleza.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.beleza.model.Usuario;
@@ -10,22 +11,28 @@ import com.beleza.repository.UsuarioRepository;
 
 @Service
 public class UsuarioService {
-	
+
 	@Autowired
 	private UsuarioRepository usuarioRepository;
 
-	public List<Usuario> listaUsuario(){
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+	public List<Usuario> listaUsuario() {
 		return usuarioRepository.findAll();
 	}
-	
+
 	public Usuario salvarUsuario(Usuario usuario) {
+		usuario.setEmail(usuario.getEmail());
+		usuario.setPerfil(usuario.getPerfil());
+		usuario.setSenha(bCryptPasswordEncoder.encode(usuario.getSenha()));
 		return usuarioRepository.save(usuario);
 	}
-	
+
 	public void deletarUsuario(Integer id) {
 		usuarioRepository.delete(id);
 	}
-	
+
 	public Usuario getById(Integer id) {
 		return usuarioRepository.findOne(id);
 	}
