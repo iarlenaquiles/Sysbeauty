6d306888666111ff76c580dd35bd6698c3950df4
Iarlen <iarlensilva775@gmail.com>
2018-02-27 17:45:12 -0300

    Criando o repositorio do reset

--- src/main/java/com/beleza/repository/ResetSenhaRepository.java
+++ src/main/java/com/beleza/repository/ResetSenhaRepository.java
@@ -1,5 +1,9 @@
 package com.beleza.repository;
 
-public interface ResetSenhaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.beleza.model.ResetSenha;
+
+public interface ResetSenhaRepository extends JpaRepository<ResetSenha, Integer>{
 
 }
