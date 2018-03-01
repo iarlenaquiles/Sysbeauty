999d4d33827e0e48eb7e2dba63bf42e6e97ad94a
Iarlen <iarlensilva775@gmail.com>
2018-03-01 15:42:14 -0300

    Criando o controller cadastrar

---
+++ src/main/java/com/beleza/controller/CadastrarController.java
@@ -0,0 +1,13 @@
+package com.beleza.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CadastrarController {
+	
+	@PostMapping("/public/cadastrarcliente")
+	public void listar() {
+		
+	}
+}
