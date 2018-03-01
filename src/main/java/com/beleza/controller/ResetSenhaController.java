cd12994eae6b17c95ca2810772ac701efcb479e2
Iarlen <iarlensilva775@gmail.com>
2018-03-01 19:55:51 -0300

    Criando o controller público do reset da senha

---
+++ src/main/java/com/beleza/controller/ResetSenhaController.java
@@ -0,0 +1,32 @@
+package com.beleza.controller;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.beleza.model.Usuario;
+import com.beleza.service.UsuarioService;
+
+@RestController
+public class ResetSenhaController {
+	
+	@Autowired
+	private UsuarioService usuarioService;
+	
+	@PostMapping("/public/resetsenha")
+	public String resetSenha(@RequestParam("email") String email) {
+		Usuario usuario = this.usuarioService.getByEmail(email);
+
+		String token = UUID.randomUUID().toString();
+		if (usuario != null) {
+			this.usuarioService.createResetSenhaUsuario(usuario, token);
+			// Criar mailsender
+			return "Email enviado";
+		}
+		return "Usuário não encontrado";
+	}
+
+}
