0a921184aaadb3ccc30ff02268465f04c5708155
Iarlen <iarlensilva775@gmail.com>
2018-03-02 14:17:05 -0300

    Criando método para pegar usuário logado

--- src/main/java/com/beleza/controller/UsuarioController.java
+++ src/main/java/com/beleza/controller/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.beleza.controller;
 
+import java.security.Principal;
 import java.util.List;
 import java.util.UUID;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.beleza.model.Usuario;
@@ -69,6 +71,14 @@ public class UsuarioController {
 		}
 
 		return null;
+	}
+	
+	@GetMapping("/usuarios/logado")
+	@ResponseBody
+	public Usuario usuarioLogado(Principal principal) {
+		Usuario usuario = this.usuarioService.getByEmail(principal.getName());
+		usuario.setSenha("");
+		return usuario;
 	}
 
 }
