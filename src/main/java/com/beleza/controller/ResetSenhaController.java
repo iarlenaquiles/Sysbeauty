a0e2e0882f57f3e987c73c1096e197dfed9b1972
Iarlen <iarlensilva775@gmail.com>
2018-03-05 14:48:27 -0300

    Ajustando o retorno do reset

--- src/main/java/com/beleza/controller/ResetSenhaController.java
+++ src/main/java/com/beleza/controller/ResetSenhaController.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.beleza.model.Usuario;
@@ -13,22 +12,21 @@ import com.beleza.service.UsuarioService;
 
 @RestController
 public class ResetSenhaController {
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
-	@PostMapping("/public/resetsenha")
-	@ResponseBody
-	public String resetSenha(@RequestParam("email") String email) {
+
+	@PostMapping(value = "/public/resetsenha")
+	public boolean resetSenha(@RequestParam("email") String email) {
 		Usuario usuario = this.usuarioService.getByEmail(email);
-		System.out.println("Email:"+email);
+		System.out.println("Email:" + email);
 		String token = UUID.randomUUID().toString();
 		if (usuario != null) {
 			this.usuarioService.createResetSenhaUsuario(usuario, token);
 			// Criar mailsender
-			return "Email enviado";
+			return true;
 		}
-		return "Usuário não encontrado";
+		return false;
 	}
 
 }
