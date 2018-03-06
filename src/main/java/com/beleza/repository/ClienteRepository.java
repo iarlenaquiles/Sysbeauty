2d3a388f7a81069423e17512d038360c98f7fec4
Iarlen <iarlensilva775@gmail.com>
2018-03-05 21:21:41 -0300

    Criando a função para pegar o cliente pelo usuario

--- src/main/java/com/beleza/repository/ClienteRepository.java
+++ src/main/java/com/beleza/repository/ClienteRepository.java
@@ -4,8 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.beleza.model.Cliente;
+import com.beleza.model.Usuario;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	Cliente getByUsuario(Usuario usuario);
 }
