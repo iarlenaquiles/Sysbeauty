72760460c25587bc2ccdf31220be6c548d914c87
Iarlen <iarlensilva775@gmail.com>
2018-03-07 19:19:27 -0300

    Criando a model da ajuda

--- src/main/java/com/beleza/model/Ajuda.java
+++ src/main/java/com/beleza/model/Ajuda.java
@@ -1,5 +1,53 @@
 package com.beleza.model;
 
-public class Ajuda {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Ajuda implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private Integer id;
+
+	private String pergunta;
+
+	private String resposta;
+
+	public Ajuda(Integer id, String pergunta, String resposta) {
+		this.id = id;
+		this.pergunta = pergunta;
+		this.resposta = resposta;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getPergunta() {
+		return pergunta;
+	}
+
+	public void setPergunta(String pergunta) {
+		this.pergunta = pergunta;
+	}
+
+	public String getResposta() {
+		return resposta;
+	}
+
+	public void setResposta(String resposta) {
+		this.resposta = resposta;
+	}
 
 }
