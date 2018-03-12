0c1fe47a0cc63f67901aa4e2eccd5cdc745ab222
Iarlen <iarlensilva775@gmail.com>
2018-03-12 16:15:29 -0300

    criando a função de configuração'

--- src/main/java/com/beleza/App.java
+++ src/main/java/com/beleza/App.java
@@ -2,14 +2,21 @@ package com.beleza;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer{
 
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(App.class);
+    }
+	
 	public static void main(String[] args) {
 		SpringApplication.run(App.class, args);
 	}
