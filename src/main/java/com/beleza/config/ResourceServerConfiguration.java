11e1a1b47b402da19c8a440a42084bc5976d95a0
Iarlen <iarlensilva775@gmail.com>
2018-03-13 16:44:24 -0300

    Configurando e protegendo as rotas

--- src/main/java/com/beleza/config/ResourceServerConfiguration.java
+++ src/main/java/com/beleza/config/ResourceServerConfiguration.java
@@ -19,8 +19,21 @@ public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
 		http.logout().logoutUrl("/oauth/logout").invalidateHttpSession(true).clearAuthentication(true).and()
-				.authorizeRequests().antMatchers("/clientes/**").hasAnyRole("CLIENTE", "PROFISSIONAL").antMatchers("/profissionais/**")
-				.hasAnyRole("PROFISSIONAL","CLIENTE").antMatchers("/portal/**").hasRole("MODERADOR").anyRequest().permitAll()
-				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll();
+				.authorizeRequests().antMatchers("/clientes/**").hasAnyRole("CLIENTE", "PROFISSIONAL", "MODERADOR")
+				.antMatchers("/profissionais/**").hasAnyRole("PROFISSIONAL", "CLIENTE", "MODERADOR")
+				.antMatchers("/agendamentos/**").hasAnyRole("PROFISSIONAL", "CLIENTE", "MODERADOR")
+				.antMatchers("/ajudas/**").hasAnyRole("PROFISSIONAL", "CLIENTE", "MODERADOR").antMatchers("/banners/**")
+				.hasAnyRole("PROFISSIONAL", "CLIENTE", "MODERADOR").antMatchers("/cancelamentos/**")
+				.hasAnyRole("PROFISSIONAL", "CLIENTE").antMatchers("/comprapacote/**")
+				.hasAnyRole("PROFISSIONAL", "CLIENTE").antMatchers("/moderadores/**").hasRole("MODERADOR")
+				.antMatchers("/notificacoes/**").hasAnyRole("PROFISSIONAL", "CLIENTE").antMatchers("/pacotes/**")
+				.hasRole("PROFISSIONAL").antMatchers("/planos/**").hasAnyRole("PROFISSIONAL", "MODERADOR")
+				.antMatchers("/portfolios/**").hasRole("PROFISSIONAL").antMatchers("/promocoes/**")
+				.hasAnyRole("PROFISSIONAL", "MODERADOR").antMatchers("/qualificacoes/**")
+				.hasAnyRole("PROFISSIONAL", "CLIENTE", "MODERADOR").antMatchers("/selos/**").hasRole("PROFISSIONAL")
+				.antMatchers("/servicos/**").hasAnyRole("PROFISSIONAL", "CLIENTE", "MODERADOR")
+				.antMatchers("/usuarios/**").hasAnyRole("PROFISSIONAL", "CLIENTE", "MODERADOR")
+				.antMatchers("/portal/**").hasRole("MODERADOR").anyRequest().permitAll()
+				.antMatchers(HttpMethod.OPTIONS, "/**").permitAll().antMatchers("/faleconosco/").permitAll();
 	}
 }
