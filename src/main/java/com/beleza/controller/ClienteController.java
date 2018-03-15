a5a20a1863faa37d474795a4c3b5933ade749786
Iarlen <iarlensilva775@gmail.com>
2018-03-14 23:10:43 -0300

    Criando a rota para fazer o upload da foto do cliente

--- src/main/java/com/beleza/controller/ClienteController.java
+++ src/main/java/com/beleza/controller/ClienteController.java
@@ -1,5 +1,6 @@
 package com.beleza.controller;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.List;
 
@@ -10,11 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.beleza.model.Cliente;
 import com.beleza.model.Usuario;
 import com.beleza.service.ClienteService;
+import com.beleza.service.UploadService;
 import com.beleza.service.UsuarioService;
 
 @RestController
@@ -22,9 +26,12 @@ public class ClienteController {
 
 	@Autowired
 	private ClienteService clienteService;
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
+
+	@Autowired
+	private UploadService uploadService;
 
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
@@ -44,7 +51,7 @@ public class ClienteController {
 	@PutMapping("/clientes")
 	public Cliente editar(@RequestBody Cliente cliente) {
 		this.usuarioService.salvarUsuario(cliente.getUsuario());
-		
+
 		return this.clienteService.salvarCliente(cliente);
 	}
 
@@ -52,12 +59,17 @@ public class ClienteController {
 	public void deletar(@PathVariable Integer id) {
 		this.clienteService.deleteCliente(id);
 	}
-	
+
 	@GetMapping("/clientes/logado")
 	public Cliente getClienteLogado(Principal principal) {
 		Usuario usuario = this.usuarioService.getByEmail(principal.getName());
-		
+
 		return this.clienteService.getByUsuario(usuario);
+	}
+
+	@PostMapping("/clientes/upload")
+	public String upload(@RequestParam("foto") MultipartFile[] foto) throws IllegalStateException, IOException {
+		return this.uploadService.uploadCliente(foto);
 	}
 
 }
