685bd0cab8958cb16017d97e32f4bd9a8ad62be1
Iarlen <iarlensilva775@gmail.com>
2018-03-23 22:12:35 -0300

    Criando a consulta para pegar a quantidade de atendimentos finalizado por um profissional

--- src/main/java/com/beleza/repository/AgendamentoRepository.java
+++ src/main/java/com/beleza/repository/AgendamentoRepository.java
@@ -3,6 +3,7 @@ package com.beleza.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.beleza.model.Agendamento;
@@ -11,4 +12,7 @@ import com.beleza.model.Profissional;
 @Repository
 public interface AgendamentoRepository extends JpaRepository<Agendamento, Integer> {
 	List<Agendamento> getByProfissional(Profissional profissional);
+
+	@Query(value = "SELECT count(*) FROM agendamento left join profissional on profissional.id = agendamento.profissional_id where profissional.id= ?1 and agendamento.status = 2", nativeQuery = true)
+	Integer quantidadeAgendamentosFinalizados(Integer idProfissional);
 }
