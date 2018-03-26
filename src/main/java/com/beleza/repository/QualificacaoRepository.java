8bfe7016e9a4a3a556e59eee1720778caf81c458
Iarlen <iarlensilva775@gmail.com>
2018-03-26 16:06:54 -0300

    Criando o método para pegar as qualificações do profissional

--- src/main/java/com/beleza/repository/QualificacaoRepository.java
+++ src/main/java/com/beleza/repository/QualificacaoRepository.java
@@ -1,9 +1,14 @@
 package com.beleza.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.beleza.model.Profissional;
 import com.beleza.model.Qualificacao;
 
 public interface QualificacaoRepository extends JpaRepository<Qualificacao, Integer>{
 
+	List<Qualificacao> getByProfissional(Profissional profissional);
+	
 }
