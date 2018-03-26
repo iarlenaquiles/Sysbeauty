db07a520dec4fc28bc1791ed3fd70e7d566c3a71
Iarlen <iarlensilva775@gmail.com>
2018-03-26 17:19:26 -0300

    Criando as consultas para pegar as qualificações dos profissionais e dos clientes

--- src/main/java/com/beleza/repository/QualificacaoRepository.java
+++ src/main/java/com/beleza/repository/QualificacaoRepository.java
@@ -3,14 +3,16 @@ package com.beleza.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-import com.beleza.model.Cliente;
-import com.beleza.model.Profissional;
 import com.beleza.model.Qualificacao;
 
-public interface QualificacaoRepository extends JpaRepository<Qualificacao, Integer>{
+public interface QualificacaoRepository extends JpaRepository<Qualificacao, Integer> {
 
-	List<Qualificacao> getByProfissional(Profissional profissional);
-	
-	List<Qualificacao> getByCliente(Cliente cliente);
+	@Query(value = "SELECT qualificacao.* FROM profissional left join qualificacao on qualificacao.profissional_id = profissional.id left join cliente on cliente.id = qualificacao.cliente_id where profissional.id = ?1", nativeQuery = true)
+	List<Qualificacao> getByProfissional(Integer id);
+
+	@Query(value = "SELECT qualificacao.* FROM cliente left join qualificacao on qualificacao.cliente_id = cliente.id left join profissional on profissional.id = qualificacao.profissional_id where cliente.id = ?1", nativeQuery=true)
+	List<Qualificacao> getByCliente(Integer id);
+
 }
