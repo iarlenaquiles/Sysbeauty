2fb1066f35176a525214c7b937b5a3344e907617
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-05-28 18:24:04 -0300

    Salvando o usuário do moderador e editando o usuário no moderador

--- src/main/java/com/beleza/controller/ModeradorController.java
+++ src/main/java/com/beleza/controller/ModeradorController.java
@@ -1,5 +1,6 @@
 package com.beleza.controller;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,23 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.beleza.model.Moderador;
+import com.beleza.model.Perfil;
+import com.beleza.model.Usuario;
 import com.beleza.service.MederadorService;
+import com.beleza.service.PerfilService;
+import com.beleza.service.UsuarioService;
 
 @RestController
 public class ModeradorController {
 
 	@Autowired
-	MederadorService moderadorService; 
+	private MederadorService moderadorService;
+
+	@Autowired
+	private PerfilService perfilService;
+
+	@Autowired
+	private UsuarioService usuarioService;
 
 	@GetMapping("/moderadores")
 	public List<Moderador> listar() {
@@ -32,11 +43,28 @@ public class ModeradorController {
 
 	@PostMapping("/moderadores")
 	public Moderador salvar(@RequestBody Moderador moderador) {
+		List<Perfil> perfis = perfilService.listaPerfil();
+
+		if (perfis.isEmpty()) {
+			perfilService.salvarPerfil(new Perfil("ROLE_CLIENTE"));
+			perfilService.salvarPerfil(new Perfil("ROLE_PROFISSIONAL"));
+			perfilService.salvarPerfil(new Perfil("ROLE_MODERADOR"));
+		}
+
+		Perfil perfil = perfilService.getByNome("ROLE_MODERADOR");
+		List<Perfil> perfiluser = new ArrayList<>();
+
+		perfiluser.add(perfil);
+
+		Usuario usuario = moderador.getUsuario();
+		usuario.setPerfil(perfiluser);
+		usuarioService.salvarUsuario(usuario);
 		return this.moderadorService.salvarModerador(moderador);
 	}
 
 	@PutMapping("/moderadores")
 	public Moderador editar(@RequestBody Moderador moderador) {
+		usuarioService.salvarUsuario(moderador.getUsuario());
 		return this.moderadorService.salvarModerador(moderador);
 	}
 
