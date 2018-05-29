6d8c89cbe6d56e0a5ba6e320d0c10705e6ee15e0
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-05-29 17:30:34 -0300

    Criando a class de constantes

---
+++ src/main/java/com/beleza/util/Constants.java
@@ -0,0 +1,10 @@
+package com.beleza.util;
+
+public class Constants {
+	
+	public static String BASE_UPLOAD = "upload";
+	
+	public static String BASE = "http://localhost:8080/upload/image/";
+	
+	//public static String BASE = "http://181.215.89.98:9999/disque/upload/image/";
+}
