bc9bcd9add2f159a4ead04a5557c334b2f89eb57
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-05-29 10:37:30 -0300

    Criando a consulta para pegar o profissional pelo nome

--- src/main/java/com/beleza/repository/ProfissionalRepository.java
+++ src/main/java/com/beleza/repository/ProfissionalRepository.java
@@ -3,6 +3,7 @@ package com.beleza.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.beleza.model.Profissional;
@@ -12,8 +13,9 @@ import com.beleza.model.Usuario;
 public interface ProfissionalRepository extends JpaRepository<Profissional, Integer> {
 
 	Profissional getByUsuario(Usuario usuario);
-	
+
 	List<Profissional> getByUsuarioStatusFalse();
-	
-	Profissional getByNome(String nome);
+
+	@Query(value = "SELECT * FROM profissional WHERE nome LIKE ?1%", nativeQuery = true)
+	List<Profissional> getByNome(String nome);
 }
