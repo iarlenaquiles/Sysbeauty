3a9ad3fa2dd6dcba97d99984e36caabc5921324d
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-05-29 11:06:01 -0300

    Criando a consulta para pegar os clientes pelo nome

--- src/main/java/com/beleza/repository/ClienteRepository.java
+++ src/main/java/com/beleza/repository/ClienteRepository.java
@@ -1,6 +1,9 @@
 package com.beleza.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.beleza.model.Cliente;
@@ -10,4 +13,7 @@ import com.beleza.model.Usuario;
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
 	Cliente getByUsuario(Usuario usuario);
+	
+	@Query(value = "SELECT * FROM cliente WHERE nome LIKE ?1%", nativeQuery = true)
+	List<Cliente> getByNome(String nome);
 }
