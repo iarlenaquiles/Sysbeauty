5959e3efa310846c2659f81c36c0740045c337de
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-05-30 14:58:22 -0300

    USando a anotação generatedvalue

--- src/main/java/com/beleza/model/Banner.java
+++ src/main/java/com/beleza/model/Banner.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Transient;
 
@@ -16,6 +18,7 @@ public class Banner implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 
 	private String nome;
