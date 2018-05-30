cc9889fe80d864c0e314d40c8428b05e2cb9118e
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-05-30 14:16:33 -0300

    Criando a condição para salvar a imagem do banner

--- src/main/java/com/beleza/controller/BannerController.java
+++ src/main/java/com/beleza/controller/BannerController.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.beleza.model.Banner;
 import com.beleza.service.BannerService;
+import com.beleza.util.Constants;
+import com.beleza.util.FileUtil;
 
 @RestController
 public class BannerController {
@@ -32,11 +34,21 @@ public class BannerController {
 
 	@PostMapping("/banners")
 	public Banner salvar(@RequestBody Banner banner) {
+		if (banner.getImagemData() != null) {
+			String base64 = banner.getImagemData();
+			FileUtil.saveFile(banner.getImagem(), base64, Constants.BASE_UPLOAD);
+			banner.setImagem(Constants.BASE + banner.getImagem());
+		}
 		return this.bannerService.salvarBanner(banner);
 	}
 
 	@PutMapping("/banners")
 	public Banner editar(@RequestBody Banner banner) {
+		if (banner.getImagemData() != null) {
+			String base64 = banner.getImagemData();
+			FileUtil.saveFile(banner.getImagem(), base64, Constants.BASE_UPLOAD);
+			banner.setImagem(Constants.BASE + banner.getImagem());
+		}
 		return this.bannerService.salvarBanner(banner);
 	}
 
