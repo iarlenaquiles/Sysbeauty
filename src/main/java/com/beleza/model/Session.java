dd5ddac1902e2f04a9669a99c905ac985c1ae497
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-06-21 16:47:49 -0300

    Criando a model da session

---
+++ src/main/java/com/beleza/model/Session.java
@@ -0,0 +1,19 @@
+package com.beleza.model;
+
+public class Session {
+
+	private String id;
+
+	public Session() {
+
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
