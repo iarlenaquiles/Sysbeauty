9d5e3cef79fa97133a3927e6f3fc646ddd7911ca
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-06-21 21:27:24 -0300

    Criando o método getsession

--- src/main/java/com/beleza/controller/PagseguroController.java
+++ src/main/java/com/beleza/controller/PagseguroController.java
@@ -1,8 +1,41 @@
 package com.beleza.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.beleza.model.Session;
+
+import br.com.uol.pagseguro.api.PagSeguro;
+import br.com.uol.pagseguro.api.PagSeguroEnv;
+import br.com.uol.pagseguro.api.credential.Credential;
+import br.com.uol.pagseguro.api.http.JSEHttpClient;
+import br.com.uol.pagseguro.api.session.CreatedSession;
+import br.com.uol.pagseguro.api.utils.logging.SimpleLoggerFactory;
 
 @RestController
 public class PagseguroController {
+
+	@Value("${pagseguro.email}")
+	private String email;
+
+	@Value("${pagseguro.token}")
+	private String token;
+	
+	@GetMapping("/pagseguro/session")
+	public Session getSessionId() {
+		final PagSeguro pagSeguro = PagSeguro.instance(new SimpleLoggerFactory(), new JSEHttpClient(),
+				Credential.sellerCredential(email, token), PagSeguroEnv.SANDBOX);
+
+		try {
+			CreatedSession createdSession = pagSeguro.sessions().create();
+			Session session = new Session();
+			session.setId(createdSession.getId());
+			return session;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
