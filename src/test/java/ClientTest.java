aab2c44541d1ab4dc4ff20d0ef271a36adba49e0
Iarlen Aquiles <iarlensilva775@gmail.com>
2018-11-12 01:19:08 -0300

    Criando o teste do cliente

---
+++ src/test/java/ClientTest.java
@@ -0,0 +1,30 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.beleza.model.Cliente;
+import com.beleza.model.Perfil;
+import com.beleza.model.Usuario;
+
+class ClientTest {
+
+	@Test
+	void test() {
+		Perfil p = new Perfil("ROLE_ADMIN");
+		List<Perfil> meuPerfil = new ArrayList<>();
+		meuPerfil.add(p);
+
+		Usuario user1 = new Usuario("iarlen@gmail.com", "123", meuPerfil);
+
+		Cliente cli1 = new Cliente();
+		cli1.setUsuario(user1);
+
+		assertEquals(user1.getEmail(), "iarlen@gmail.com");
+		assertEquals("ROLE_ADMIN", user1.getPerfil().get(0).getNome());
+		assert (cli1 == null);
+	}
+
+}
